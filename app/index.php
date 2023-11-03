7840fecc157070e3a0433d9f20e29e6aaf527517
Carlos Manuel Cedeño Barrera <carlosmcb02@gmail.com>
2023-11-03 08:31:49 -0400

    commit

---
+++ app/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Docker Compose</title>
+</head>
+<body>
+    <h1>En esta practica usaremos Docker Compose</h1>
+    <h4>Carlos Manuel Cedeño Barrera 2022-0018</h4>
+    <?php
+        echo "<h2><bold>Hola Mundo usando Docker Compose</bold></h2>";
+    
+    ?>
+</body>
+</html>
